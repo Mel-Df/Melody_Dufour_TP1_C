3103aa2fc7cf8d2f5793ba07c7c15dca516f7dbf
Mel <melody.dufour@outlook.fr>
2020-01-13 13:04:17 +0100

    Create test.C

---
+++ test.C
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+
+int main() {
+int joueur;
+int pvmonstre;
+int pvjoueur;
+int monstre;
+
+
+pvmonstre= 5;
+attaque= pvmonstre-1;
+
+printf("Le monstre possede %d PV", pvmonstre);
+
+while (pvmonstre= 5){
+ return attaque; 
+}
+
+if (pvmonstre= 0){
+printf("Vous avez battu le monstre!");
+}
+
+
+
+return 0;	
+}
